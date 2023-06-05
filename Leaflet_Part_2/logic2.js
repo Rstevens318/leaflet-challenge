5ee246f1653a8bfb6e025a0aa0987ac25373542a
Robert  Stevens <Rstevens318@gmail.com>
2023-06-04 20:23:06 -0400

    Reworked Pt. 1. Working Pt. 2

--- Leaflet_Part_2/logic2.js
+++ Leaflet_Part_2/logic2.js
@@ -3,6 +3,7 @@
 
       // Get GeoJson data for earthquakes within the last 7 days
       let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+      let platesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
       d3.json(url).then(function(response) {
         createMarkers(response.features);
@@ -62,6 +63,7 @@
     
 
     function createMapWithLayer(mapping) {
+    
       // Add a tile layer (the background map image) from MapBox
       let grayscale = L.tileLayer('https://api.mapbox.com/styles/v1/{style}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicnN0ZXZlbnMzMTgiLCJhIjoiY2xpaTJqM2ZpMHZ3eTNmbXE4YjFtdjVqZyJ9.hT8YYeWHPYyqgqlwsUUZMA', {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -71,10 +73,49 @@
         style: 'mapbox/light-v11',
       });
 
+      let outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/{style}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicnN0ZXZlbnMzMTgiLCJhIjoiY2xpaTJqM2ZpMHZ3eTNmbXE4YjFtdjVqZyJ9.hT8YYeWHPYyqgqlwsUUZMA', {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        maxZoom: 18,
+        tileSize: 512,
+        zoomOffset: -1,
+        style: 'mapbox/outdoors-v12',
+      });
+
+      let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{style}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicnN0ZXZlbnMzMTgiLCJhIjoiY2xpaTJqM2ZpMHZ3eTNmbXE4YjFtdjVqZyJ9.hT8YYeWHPYyqgqlwsUUZMA', {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        maxZoom: 18,
+        tileSize: 512,
+        zoomOffset: -1,
+        style: 'mapbox/satellite-v9',
+      });
+  // New Layer Group for Tectonic plate data
+  faultLines = new L.LayerGroup();
+
+  // Get Tectonic Plate GeoJson data
+  d3.json(platesUrl).then(function(plates) {
+    console.log(plates);
+    L.geoJSON(plates, {
+      color: "orange",
+      weight: 2
+    }).addTo(faultLines);
+  });
+
+      let baseMaps = {
+        "Grayscale": grayscale,
+        "Outdoors": outdoors,
+        "Satellite": satellite
+      };
+
+      // Create overlay object to hold our overlay layer
+      let overlayMaps = {
+        "Earthquakes": mapping,
+        "Tectonic Plates": faultLines
+      };
+
       let myMap = L.map("map", {
         center: [37.09, -95.71],
         zoom: 4,
-        layers: [mapping, grayscale]
+        layers: [mapping, grayscale, plates]
       });
 
   
@@ -105,4 +146,9 @@
     return div;
 };
 legend.addTo(myMap);
-      }
+
+      // Create a layer control and add to the map
+      L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+      }).addTo(myMap);
+    };
